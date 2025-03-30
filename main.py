245cbe28f3354a9746ace59063869153a688607d
servinfcolombia <servinfcolombia@gmail.com>
2025-03-30 17:48:30 -0500

    Le agregue la funcion para confirmar que la conexion a la base de datos es correcta.

--- main.py
+++ main.py
@@ -51,7 +51,24 @@ def read_root():
 
 @app.get("/test")
 def read_test():
-    return {"message": "This is a test endpoint"}
+    try:
+        connection = mysql.connector.connect(**db_config)
+        cursor = connection.cursor(dictionary=True)
+
+        # Ejecutar una consulta de prueba
+        cursor.execute("SELECT 1")
+        result = cursor.fetchone()
+
+        print("Conexión exitosa a la base de datos. Resultado de la consulta:", result)
+
+    except mysql.connector.Error as err:
+        print("Error al conectarse a la base de datos:", err)
+
+    finally:
+        if connection.is_connected():
+            cursor.close()
+            connection.close()
+            print("Conexión cerrada.")
 
 # Ejecutar la aplicación
 if __name__ == "__main__":
